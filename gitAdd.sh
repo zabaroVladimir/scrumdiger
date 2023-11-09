92c65914bebdab3ecb99f289daa90e62017f4bae
zabarik <55857050+zabaroVladimir@users.noreply.github.com>
2023-11-09 10:47:27 +0100

    comm

---
+++ gitAdd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# git add
+git add .
+
+# commit
+git commit -m "comm"
+
+#push
+git push https://zabarik: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@github.com/zabaroVladimir/Swift2023.git
+
+#status
+git status
---
+++ scrumdiger/scrumdigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdigerApp.swift
+//  scrumdiger
+//
+//  Created by Vladimir Zabaro on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct scrumdigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
